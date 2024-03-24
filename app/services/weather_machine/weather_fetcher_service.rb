63909a65340b72c18b291a03f73cf4f2d44101d3
Thomas DoyleEngler <tpdoyle87@gmail.com>
2024-03-24 15:52:14 -0400

    service to fetch weather and return 7 day temp forecast

--- app/services/weather_machine/weather_fetcher_service.rb
+++ app/services/weather_machine/weather_fetcher_service.rb
@@ -19,57 +19,95 @@ module WeatherMachine
 
     def call
       # returns an object containing the endpoints for the nearby weather stations and forecast.
-      response = RestClient.get("https://api.weather.gov/points/#{latitude},#{longitude}")
-      local_grid = JSON.parse(response.body)
-      forecast = RestClient.get(local_grid['properties']['forecast'])
-      forecast_data = JSON.parse(forecast.body)
+      local_grid = fetch_grid
+      # returns the forecast data for the next 7 days.
+      forecast_data = fetch_seven_day_forecast(local_grid)
+      # builds the forecast object with the current temperature and the high and low temperatures for the next 7 days.
       build_forecast(forecast_data)
+      forecast
     end
 
     def build_forecast(forecast_data)
       # Assume the first period has the current temperature
-      current_period = weather_data[:properties][:periods].first
-      forecast[:currentTemperature] = {
-        temperature: current_period[:temperature],
-        unit: current_period[:temperatureUnit]
+      current_period = forecast_data['properties']['periods'].first
+      forecast[:current_temperature] = {
+        temperature: current_period['temperature'],
+        unit: current_period['temperatureUnit']
       }
-      first_daily_forecast_index(forecast_data)
+      not_current_day_index = first_daily_forecast_index(forecast_data)
+      build_current_day_forecast(forecast_data, not_current_day_index, forecast)
+      forecast_for_week(forecast_data, not_current_day_index, forecast)
     end
 
     def first_daily_forecast_index(forecast_data)
-      forecast_data[:properties][:periods].index do |period|
-        DAYS_OF_THE_WEEK.any? { |day| period[:name].start_with?(day) }
+      forecast_data['properties']['periods'].index do |period|
+        DAYS_OF_THE_WEEK.any? { |day| period['name'].start_with?(day) }
       end
+    end
+
+    def build_current_day_forecast(forecast_data, end_index, forecast)
+      return unless end_index
+
+      high_period = forecast_data['properties']['periods'][0]
+      low_period = forecast_data['properties']['periods'][0]
+
+      high_period, low_period = find_high_low_temp_period(end_index, forecast_data, high_period, low_period)
+      build_current_day(forecast, high_period, low_period)
     end
 
     def forecast_for_week(forecast_data, start_index, forecast)
       return unless start_index
 
-      forecast_data[:properties][:periods][start_index..].each_with_index do |period, index|
+      forecast_data['properties']['periods'][start_index..].each_with_index do |period, index|
         if index.even?
-          forecast[:nextSevenDays] << build_high_temp(period)
+          forecast[:next_seven_days] << build_high_temp(period)
         else
-          forecast[:nextSevenDays].last.merge!(build_low_temp(period))
+          forecast[:next_seven_days].last.merge!(build_low_temp(period))
         end
 
-        break if forecast[:nextSevenDays].size == 7
+        break if forecast[:next_seven_days].size == 7
       end
     end
 
     private
 
+    def find_high_low_temp_period(end_index, forecast_data, high_period, low_period)
+      forecast_data['properties']['periods'][0..end_index].each_with_index do |period, index|
+        if period['temperature'] > high_period['temperature']
+          high_period = forecast_data['properties']['periods'][index]
+        end
+        low_period = forecast_data['properties']['periods'][index] if period['temperature'] < low_period['temperature']
+      end
+      [high_period, low_period]
+    end
+
+    def build_current_day(forecast, high_period, low_period)
+      forecast[:next_seven_days] << build_high_temp(high_period)
+      forecast[:next_seven_days].last.merge!(build_low_temp(low_period))
+    end
+
+    def fetch_seven_day_forecast(local_grid)
+      response = RestClient.get(local_grid['properties']['forecast'])
+      JSON.parse(response.body)
+    end
+
+    def fetch_grid
+      response = RestClient.get("https://api.weather.gov/points/#{latitude},#{longitude}")
+      JSON.parse(response.body)
+    end
+
     def build_low_temp(period)
       {
-        lowTemperature: period[:temperature],
-        lowTemperatureUnit: period[:temperatureUnit]
+        lowTemperature: period['temperature'],
+        lowTemperatureUnit: period['temperatureUnit']
       }
     end
 
     def build_high_temp(period)
       {
-        day: period[:name],
-        highTemperature: period[:temperature],
-        highTemperatureUnit: period[:temperatureUnit]
+        day: period['name'],
+        highTemperature: period['temperature'],
+        highTemperatureUnit: period['temperatureUnit']
       }
     end
   end
